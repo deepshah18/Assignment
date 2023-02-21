bfe15bda70c8dda286f14f606ca739403924a0e1
Deep <66822101+deepshah18@users.noreply.github.com>
2023-02-21 04:28:17 -0500

    Update Task3.rb

--- Assign5/Task3.rb
+++ Assign5/Task3.rb
@@ -2,15 +2,15 @@ require 'date'
 
 
 def date(startyear,endyear,date)
-date1=DateTime.parse(date)
-year1=date1.year
-for year1 in (startyear..endyear)
-  puts "Start Year:#{startyear}"
-  puts "End Year:#{endyear}"
-  puts "#{date}"
-  puts "Yes, #{date1.strftime('%d-%m-%Y')} is between 01-01-#{startyear} to 01-01-#{endyear}"
-  break
-end
+  date1=DateTime.parse(date)
+  year1=date1.year
+  for year1 in (startyear..endyear)
+    puts "Start Year:#{startyear}"
+    puts "End Year:#{endyear}"
+    puts "#{date}"
+    puts "Yes, #{date1.strftime('%d-%m-%Y')} is between 01-01-#{startyear} to 01-01-#{endyear}"
+    break
+  end
 end
 
 date(2010,2020,"Tue, 10 Aug 2010")
