2886b2faf6c2eedc379a5b2683c18641b912a92e
Deep <66822101+deepshah18@users.noreply.github.com>
2023-02-24 09:50:33 +0530

    Update Task1.rb

--- Assign1/Task1.rb
+++ Assign1/Task1.rb
@@ -6,31 +6,31 @@ class Product
   @pname=name
   @pprice=price
   end
-  def create()
+  def create
      puts "enter your name"
       @pn=gets
       puts "enter gst"
       @p=gets
    end
 
-   def calc()
-     final=@pprice.to_i;
-     final1=@p.to_i;
-     total=final * final1 / 100;
-     @answer=@pprice + total;
+   def calc
+     final=@pprice.to_i
+     final1=@p.to_i
+     total=final * final1 / 100
+     @answer=@pprice + total
    end
-   def get_user()
-     create()
-     calc()
+   def get_user
+     create
+     calc
      puts "Welcome to our #{COMPANY_NAME}"
      puts "Welcome #{@pn}. Your invoice with #{@p}% gst are below"
      puts "Product details"
      puts "#{$productcompany} :: #{@pname} - #{@answer}" 
      end
    begin 
-      product1=Product.new('keyboard',100);
+      product1=Product.new('keyboard',100)
       product1.get_user
-      product2=Product.new('mouse',200);
+      product2=Product.new('mouse',200)
       product2.get_user
    end
 end
