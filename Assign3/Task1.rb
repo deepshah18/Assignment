3c3803880e69edd8451fa1cb34ad7ae024f6de8c
Deep <66822101+deepshah18@users.noreply.github.com>
2023-02-25 10:55:58 +0530

    Update Task1.rb

--- Assign3/Task1.rb
+++ Assign3/Task1.rb
@@ -1,4 +1,5 @@
 EXPECTED_MINUTES_IN_OVEN = 40
+CONSTANT_TWO=2
 class Lasagna
  
 
@@ -6,7 +7,7 @@ class Lasagna
      return EXPECTED_MINUTES_IN_OVEN - time
   end
   def preparation_time_in_minutes(layer)
-      return layer*2
+      return layer*CONSTANT_TWO
   end 
   def total_time_in_minutes(layer,time)
       result= remaining_minutes_in_oven(time) + preparation_time_in_minutes(layer)
